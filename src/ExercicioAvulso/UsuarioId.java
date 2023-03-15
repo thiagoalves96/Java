b82fd6aaeb3fafbd51565f8f45c3b95433500cfe
thiagoalves96 <thiagodev22@hotmail.com>
2023-03-15 18:40:27 -0300

    Update

---
+++ src/ExercicioAvulso/UsuarioId.java
@@ -0,0 +1,54 @@
+package ExercicioAvulso;
+
+import java.util.Scanner;
+
+public class UsuarioId {
+	public static void main(String[] args) {
+	try {
+		Scanner sc = new Scanner(System.in);
+		int[] codigo;
+		String[] usuario;
+		int continuar;
+		
+		do {
+			System.out.println("Digite a quantidade de usuarios a serem registrada: ");
+			int quantidade = sc.nextInt();
+			usuario = new String[quantidade];
+			codigo = new int[quantidade];
+			if (quantidade == 1) {
+				System.out.println("Digite o nome do usuario " + 1 + ": ");
+				usuario[0] = sc.next();
+				System.out.println("Digite o codigo do usuario " + usuario[0] + ": ");
+				codigo[0] = sc.nextInt();
+				System.out.println(usuario[0]);
+				System.out.println(codigo[0]);
+			} else {
+				for (int i = 0; i < quantidade; i++) {
+					System.out.println("Digite o nome do usuario " + (i + 1) + ": ");
+					usuario[i] = sc.next();
+					System.out.println("Digite o codigo do usuario " + usuario[i] + ": ");
+					codigo[i] = sc.nextInt();
+				}
+				int x = quantidade - 1;
+				for (int i = 0; i < quantidade; i++) {
+					if (codigo[i] == codigo[x]) {
+						System.out.println("O codigo do " + usuario[i] + " e igual ao do outro usuario.");
+					} else {
+						System.out.println(usuario[i]);
+						System.out.println(codigo[i]);
+						x--;
+					}
+				}
+			}
+			System.out.println("Deseja continuar? (sim-1/nao-2): ");
+			continuar = sc.nextInt();
+		} while (continuar != 2);
+		sc.close();
+
+	}catch(
+
+	Exception e)
+	{
+	   System.out.println(e);
+   }
+}}
--- src/exerciciosFaccat/ReajusteSalario.java
+++ src/exerciciosFaccat/ReajusteSalario.java
@@ -8,18 +8,18 @@ public class ReajusteSalario {
 			Scanner sc = new Scanner(System.in);
 
 			System.out.print("Digite o valor do salario do funcionario: ");
-			double salario = sc.nextDouble();
+			double salarioAtual = sc.nextDouble();
 			System.out.print("Digite a porcentagem de aumento do salario: ");
 			double porcento = sc.nextDouble();
 
-			double aumento = (salario * (porcento / 100)) + salario;
+			double reajuste = salarioAtual * (porcento / 100) + salarioAtual;
 
-			System.out.println("O novo salario teve um aumento de " + String.format("%.2f", porcento) + "% e com este reajuste o "
-					+ "novo salario ficou: " + String.format("%.2f", aumento));
+			System.out.println("O novo salario teve um aumento de " + String.format("%.2f", porcento)
+					+ "% e com este reajuste o " + "novo salario ficou: " + String.format("%.2f", reajuste));
 			sc.close();
-			
+
 		} catch (Exception e) {
-			System.out.println("Digite numeros validos // "+e);
+			System.out.println("Digite numeros validos // " + e);
 		}
 
 	}
